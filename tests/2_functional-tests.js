9d7b18e0182ac5df42bd1fff59663cc31e53d1d5
Edison-Mondal <mondaledison@gmail.com>
2023-09-18 09:16:38 +0600

    ok

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -239,7 +239,9 @@ suite('Functional Tests', function() {
         assert.equal(res.status, 200);
         console.log('body11 for invalid _id -'+res.body.error+ ' _id ' + res.body._id)
         assert.property(res.body, 'error');
+        assert.property(res.body, '_id');
         assert.equal(res.body.error, 'could not update');
+        assert.equal(res.body._id, res.body._id);
         done();
       });
   });
